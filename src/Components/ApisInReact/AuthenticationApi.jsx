145585c3ef81f37ede2091dba71524fe8286bf3f
suhaas3 <suhaas3w@gmail.com>
2026-07-17 11:42:39 +0530

    APi authenticaion practice

--- src/Components/ApisInReact/AuthenticationApi.jsx
+++ src/Components/ApisInReact/AuthenticationApi.jsx
@@ -13,108 +13,137 @@ export default function AuthenticationApi() {
 
   const [loading, setLoading] = useState(false)
 
-  // const handleUserDetails = (e) => {
-
-  //   const { name, value } = e.target;
-  //   setUserDetails(prev => ({
-  //     ...prev,
-  //     [name]: value
-  //   }))
-  // }
-
-  // const data = {
-  //   email: userDetails.username,
-  //   password: userDetails.password
-  // }
-
-  // const apiData = async () => {
-  //   const userData = await axios.post("https://api.escuelajs.co/api/v1/auth/login", data)
-  // }
+  
 
 
 
-
-  // const getUserData = async () => {
-  //   setLoading(true)
-  //   try {
-  //     const response = await axios.get("https://api.escuelajs.co/api/v1/auth/profile", {
-  //       headers: {
-  //         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY3Mjc2NjAyOCwiZXhwIjoxNjc0NDk0MDI4fQ.kCak9sLJr74frSRVQp0_27BY4iBCgQSmoT3vQVWKzJg"
-  //       }
-  //     }
-  //     );
-
-  //     console.log(response, "user dataaaaaaaaaaaa");
-
-  //     setLoading(false)
-  //   } catch (err) {
-  //     console.log(err.response.data.message, "Error")
+  //   const apiData = async () => {
+  //     const userData = await axios.post("https://api.escuelajs.co/api/v1/auth/login", data)
   //   }
 
 
+
+
+  //   const getUserData = async () => {
+  //     setLoading(true)
+  //     try {
+  //       const response = await axios.get("https://api.escuelajs.co/api/v1/auth/profile", {
+  //         headers: {
+  //           "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY3Mjc2NjAyOCwiZXhwIjoxNjc0NDk0MDI4fQ.kCak9sLJr74frSRVQp0_27BY4iBCgQSmoT3vQVWKzJg"
+  //         }
+  //       }
+  //       );
+
+  //       console.log(response, "user dataaaaaaaaaaaa");
+
+  //       setLoading(false)
+  //     } catch (err) {
+  //       console.log(err.response.data.message, "Error")
+  //     }
+
+
+  //   }
+
+  //   const getData = async () => {
+  //     setLoading(true)
+  //     try {
+  //       const response = await axios.get("https://api.escuelajs.co/api/v1/products")
+  //       setProductsData(response.data)
+  //       setLoading(false)
+  //     } catch (err) {
+  //       console.log(err.response.data.message, "Error");
+  //     }
+  //   }
+
+  //   if (loading) {
+  //     return <h1>Loading....</h1>
+  //   }
+
+  //   const createProduct = async () => {
+  //     try {
+  //     const sendData = await axios.post("https://api.escuelajs.co/api/v1/products/",{
+  //     title: `New Product ${Date.now}`,
+  //     price: 10,
+  //     description: "A description",
+  //     categoryId: 2,
+  //     images: ["https://placehold.co/600x400"]
+  //   })
+
+  //   console.log(sendData.data,"created dataaaaaaa")
+  //   } catch (err) {
+  //     console.log(err.response.data.message,"Error")
+  //   }
+  //   }
+
+  //   const updateProduct = async () => {
+  //     setLoading(true)
+  //     try {
+  //       const response = await axios.put("https://api.escuelajs.co/api/v1/products/24", {
+  //         "title": `Change title  ${Date.now(ch)}`,
+  //         "price": 100
+  //       })
+
+  //       setLoading(false)
+  //     } catch (err) {
+  //       console.log(err,"Error")
+  //     }
   // }
 
-  const getData = async () => {
-    setLoading(true)
-    try {
-      const response = await axios.get("https://api.escuelajs.co/api/v1/products")
-      setProductsData(response.data)
-      setLoading(false)
-    } catch (err) {
-      console.log(err.response.data.message, "Error");
-    }
+  const handleUserDetails = (e) => {
+
+    const { name, value } = e.target;
+    setUserDetails(prev => ({
+      ...prev,
+      [name]: value
+    }))
   }
 
-  if (loading) {
-    return <h1>Loading....</h1>
+  const data = {
+    email: userDetails.username,
+    password: userDetails.password
   }
 
-  // const createProduct = async () => {
-  //   try {
-  //   const sendData = await axios.post("https://api.escuelajs.co/api/v1/products/",{
-  //   title: `New Product ${Date.now}`,
-  //   price: 10,
-  //   description: "A description",
-  //   categoryId: 2,
-  //   images: ["https://placehold.co/600x400"]
-  // })
+  const createUser = async () => {
+    const response = await axios.post("https://api.escuelajs.co/api/v1/auth/login",data)
 
-  // console.log(sendData.data,"created dataaaaaaa")
-  // } catch (err) {
-  //   console.log(err.response.data.message,"Error")
-  // }
-  // }
+    console.log(response.data.access_token,"datyaaaaaaaaaaa")
 
-  const updateProduct = async () => {
-    setLoading(true)
-    try {
-      const response = await axios.put("https://api.escuelajs.co/api/v1/products/24", {
-        "title": `Change title  ${Date.now()}`,
-        "price": 100
-      })
+    localStorage.setItem("access_key",response.data.access_token)
+  }
 
-      setLoading(false)
-    } catch (err) {
-      console.log(err,"Error")
-    }
-}
+  const userVerify = async () => {
+
+    const verifyToken = localStorage.getItem("access_key");
+
+    console.log(verifyToken,"tokennnnnnnnnnnnnnnnnn")
+
+    const response = await axios.get("https://api.escuelajs.co/api/v1/auth/profile",{
+      headers:{
+        Authorization: `Bearer ${verifyToken}`
+      }
+    })
+
+    console.log(response.data,"user dataaaaaaaaaaaaaaaa")
+  }
 
   return (
     <>
 
-      {/* <input type='text' onChange={handleUserDetails} name='username' />
+      <input type='text' onChange={handleUserDetails} name='username' />
 
-      <input type='text' onChange={handleUserDetails} name='password' /> */}
+      <input type='text' onChange={handleUserDetails} name='password' />
 
-      {/* <button onClick={getUserData}>get user data</button> */}
+      <button onClick={createUser}>create user data</button>
 
-      <button onClick={getData}>get products</button>
+      <button onClick={userVerify}>get user data</button>
+
+      {/* <button onClick={getData}>get products</button> */}
 
       {/* <button onClick={createProduct}>Cretate product</button> */}
 
-      <button onClick={updateProduct}>Update product</button>
+      {/* <button onClick={updateProduct}>Update product</button> */}
 
-      {
+      {/* {
         productsData.length > 0 &&
         productsData.map((product) => (
           <div className="product-card">
@@ -145,7 +174,9 @@ export default function AuthenticationApi() {
             </div>
           </div>
         ))
-      }
+      } */}
+
+
 
 
 
