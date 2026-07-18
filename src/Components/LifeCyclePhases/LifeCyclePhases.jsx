55f9c3b196a5127b2b5260d684abe9413dee67aa
suhaas3 <suhaas3w@gmail.com>
2026-07-18 09:38:43 +0530

    price and category filter is created

--- src/Components/LifeCyclePhases/LifeCyclePhases.jsx
+++ src/Components/LifeCyclePhases/LifeCyclePhases.jsx
@@ -5,19 +5,26 @@ import React, { useEffect, useState } from 'react'
 export default function LifeCyclePhases() {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   // const [selectCategory, setSelectCategory] = useState({
   //   category: ""
   // })
   const [selectCategory, setSelectCategory] = useState("")
   const [filteredProducts, setFilterProducts] = useState([])
 
+  //price filter state
+  const [selectedPrices, setSelectedPrices] = useState([]);
+
   useEffect(() => {
     getProducts();
   }, [])
 
-   const getProducts = async () => {
+  const getProducts = async () => {
+    setLoading(true);
     const response = await axios.get("https://dummyjson.com/products");
     setProducts(response.data.products)
+
+    setLoading(false);
   }
 
   const handleSelect = (e) => {
@@ -31,17 +38,46 @@ export default function LifeCyclePhases() {
   }
 
   useEffect(() => {
-      if (selectCategory === "") {
-        setFilterProducts(products);
-      } else {
-        const items = products.filter(product => {
-          return product.category === selectCategory
-        })
 
-        setFilterProducts(items);
-      }
-  }, [products, selectCategory]);
+    const filteredItems = products.filter(product => {
 
+      //category filter
+      const categoryFilter = selectCategory === "" || product.category === selectCategory;
+
+      //price filter
+      const priceFilter = selectedPrices.length === 0 || selectedPrices.some(range => {
+        const [min, max] = range.split("-");
+
+        return (
+          product.price >= Number(min) && product.price <= Number(max)
+        )
+      })
+
+      return categoryFilter && priceFilter;
+    })
+    setFilterProducts(filteredItems)
+  }, [products, selectCategory, selectedPrices])
+
+
+
+  if (loading) {
+    return <h1>Loading.........</h1>
+  }
+
+  const handlePrice = (e) => {
+
+    const { value, checked } = e.target;
+    if (checked) {
+      setSelectedPrices(prev => [...prev, value])
+    } else {
+      setSelectedPrices(prev =>
+        prev.filter(item => item !== value)
+      )
+    }
+
+  }
+
+  console.log(selectedPrices, "selected pricesssss")
   return (
     <>
       <h1>life cycle phases in react js</h1>
@@ -56,25 +92,49 @@ export default function LifeCyclePhases() {
         <option value="groceries">groceries</option>
       </select>
 
+      <label>Price:
+
+        <input type='checkbox' value="5-10" onChange={handlePrice} />₹5 - ₹10
+      </label>
+      <label>
+        <input type='checkbox' value="10-20" onChange={handlePrice} />₹10 - ₹20
+      </label>
+      <label>
+        <input type='checkbox' value="20-30" onChange={handlePrice} />₹20 - ₹30
+      </label>
+      <label>
+        <input type='checkbox' value="30-300" onChange={handlePrice} />₹30 - ₹300
+      </label>
+
+      <label>
+        <input type='checkbox' value="300-1000" onChange={handlePrice} />₹300 - ₹1000
+      </label>
+
+
+
+
       <div className='container-fluid'>
         <div className='row'>
 
           {filteredProducts.map((product, index) => {
+            const { title, category, price, discountPercentage, rating, tags } = product;
             return (
               <>
                 <div className='col-2' style={{ border: "solid 2px black", margin: "2px" }}>
-                  <h1 key={product.index}>{product.title}</h1>
-                  <h2>{product.category}</h2>
-                  <h3>{product.price}</h3>
-                  <h3>{product.discountPercentage}</h3>
-                  <h3>{product.rating}</h3>
-                  {product.tags.map(tag => {
+                  <h1 key={index}>{title}</h1>
+                  <h2>category: {category}</h2>
+                  <h3>price: {price}</h3>
+                  <h3>discout: {discountPercentage}</h3>
+                  <h3>rating: {rating}</h3>
+                  <h3>Tags: {tags.join(", ")}</h3>
+
+                  {/* {tags.map(tag => {
                     return (
                       <>
-                        <h1>{tag}</h1>
+                        <h3>tag :{tag}</h3>
                       </>
                     )
-                  })}
+                  })} */}
 
                 </div>
               </>
